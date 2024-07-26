bb063369e498e0db5ed2e846ce4aded863968b3f
Your Name <ekka.beaula@gmail.com>
2024-07-26 12:35:00 +0200

    initial installation

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <div>
+    <h1>index page</h1>
+    <Button variant="default">Press me</Button>
+  </div>
+</template>
+
+<style scoped></style>
